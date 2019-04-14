676eeaf506e93f6adc68720c357cb9c2425c9892
Annmary12 <annmaryamaka@yahoo.com>
2019-04-14 15:06:15 +0100

    add button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,33 @@
+// react libraires
+import React from 'react';
+
+// third-party labraries
+import PropTypes from 'prop-types';
+
+// style
+import './Button.scss';
+
+const Button = ({
+  type,
+  disabled,
+  classes,
+  name
+}) => (
+  <button
+    className={classes}
+    type={type ? type : 'button'}
+    disabled={disabled}
+  >
+    {name}
+  </button>
+)
+
+Button.propTypes = {
+  type: PropTypes.string,
+  disabled: PropTypes.bool.isRequired,
+  classes: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Button;
+
--- src/components/GenerateNumber/index.jsx
+++ src/components/GenerateNumber/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 // component
 import InputBox from '../InputBox';
+import Button from '../Button';
+
 // style
 import './GenerateNumber.scss';
 
@@ -10,6 +12,13 @@ const GenerateNumber = () => (
   <div className="section">
     <InputBox
       placeholder="Input Number"
+    />
+    <Button
+      classes="btn btn__primary"
+      type="submit"
+      name="Generate"
+      // name={isSubmitting ? 'loading..' : 'Generate'}
+      // disabled={isSubmitting}
     />
   </div>
 )
