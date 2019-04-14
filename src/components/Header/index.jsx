5250fa6bbb87809e020b7df44c2fbe607902ab89
Annmary12 <annmaryamaka@yahoo.com>
2019-04-14 13:49:02 +0100

    add header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+// react library
+import React from 'react';
+
+// style
+import './Header.scss';
+
+const Header = () => (
+   <div className="header">
+      <h1>Phone Number Generator</h1>
+   </div>
+ )
+
+ export default Header;
---
+++ src/components/Page/index.jsx
@@ -0,0 +1,13 @@
+// react library
+import React from 'react';
+
+// components
+import Header from '../Header';
+
+const Page = () => (
+  <React.Fragment>
+    <Header />
+  </React.Fragment>
+)
+
+export default Page;
