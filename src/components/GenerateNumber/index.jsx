3f3ade43eb664a5a97b15a7e0e820f28dfd7017a
Annmary12 <annmaryamaka@yahoo.com>
2019-04-15 10:38:39 +0100

    implement formik on the form

--- src/components/GenerateNumber/index.jsx
+++ src/components/GenerateNumber/index.jsx
@@ -1,5 +1,8 @@
 // react library
-import React from 'react';
+import React, { Component } from 'react';
+
+// third-party library
+import { withFormik } from 'formik';
 
 // component
 import InputBox from '../InputBox';
@@ -9,21 +12,57 @@ import Table from '../Table';
 // style
 import './GenerateNumber.scss';
 
-const GenerateNumber = () => (
-  <div className="section">
-    <InputBox
-      placeholder="Input Number"
-    />
-    <Button
-      classes="btn btn__primary"
-      type="submit"
-      name="Generate"
-      // name={isSubmitting ? 'loading..' : 'Generate'}
-      // disabled={isSubmitting}
-    />
-    <Table />
+export const GenerateNumberConfig = {
+  mapPropsToValues: () => ({
+    number: '',
+  }),
 
-  </div>
-)
+ /**
+   * handles company form submit event
+   *
+   * @param {object} values
+   * @returns {void}
+   */
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  }
+}
 
-export default GenerateNumber;
+class GenerateNumber extends Component{
+  render() {
+   const {
+      values,
+      touched,
+      errors,
+      isSubmitting,
+      handleChange,
+      handleSubmit
+    } = this.props;
+
+    return (
+      <div className="section">
+      <form onSubmit={handleSubmit}>
+        <InputBox
+          placeholder="Input Number"
+          type="number"
+          name="number"
+          { ...this.props }
+        />
+        <Button
+          classes="btn btn__primary"
+          type="submit"
+          name="Generate"
+          name={isSubmitting ? 'Generating...' : 'Generate'}
+          disabled={isSubmitting}
+        />
+      </form>
+      <Table />
+    </div>
+    )
+  }
+}
+
+export default withFormik(GenerateNumberConfig)(GenerateNumber);
