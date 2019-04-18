a9d80e74f3cccf694435774e9373e132efd43236
Annmary12 <annmaryamaka@yahoo.com>
2019-04-18 12:57:51 +0100

    restrict numbers that will be generated

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,5 +1,7 @@
 import * as Yup from 'yup';
 
 export const GenerateNumberSchema = Yup.object().shape({
-  number: Yup.number().required('Input a number').integer('number must be an integer'),
+  number: Yup.number().required('Input a number')
+    .integer('number must be an integer')
+    .max(100, 'Can\'t generate more than 100 numbers'),
 });
